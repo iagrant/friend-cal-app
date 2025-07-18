9162490beb840267f397d61d82926ffee5d7b8ab
Ian Grant <iagrant14@gmail.com>
2025-07-18 15:16:46 -0700

    make schemea match live

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE events (
 CREATE TABLE attendees (
     id SERIAL PRIMARY KEY,
     event_id INTEGER NOT NULL REFERENCES events(id) ON DELETE CASCADE,
-    user_id TEXT NOT NULL,
+    user_id text DEFAULT ''::text NOT NULL,
     name TEXT NOT NULL,
     email TEXT NOT NULL,
     UNIQUE(event_id, user_id)
